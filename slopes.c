73076fdca6bab7ed8ce06d049dd42c10d7392467
andresfgarcia150 <andrespipe150@gmail.com>
2013-04-10 23:17:39 -0500

    Pendiente V1.0

---
+++ slopes.c
@@ -0,0 +1,12 @@
+// Universidad de los Andes
+// Física computacional
+// Tarea 6
+// Archivo con el cálculo de las pendientes
+// Autores:
+//	Andrés Felipe García Albarracín
+//	Andrea Rozo
+
+double slopex(double x, double y, double z, double h)
+{
+
+}
