4be6f5346ad262ea25ebdcc18be2602ab2a1f829
Andrea Rozo <ca.rozo2200@uniandes.edu.co>
2013-04-10 23:27:43 -0500

    Derivadas

---
+++ functions.c
@@ -0,0 +1,30 @@
+// Universidad de los Andes
+// Física computacional
+// Tarea 6
+// Archivo con el cálculo de las pendientes
+// Autores:
+//	Andrés Felipe García Albarracín
+//	Andrea Rozo Méndez
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+#define sigma 10
+#define rho 28.0
+#define beta 8.0/3.0
+
+double functionX (double x, double y, double z)
+{
+       return (sigma*(y-x));
+}
+
+double functionY (double x, double y, double z)
+{
+       return (x*(rho-z)-y);
+}
+
+double functionZ (double x, double y, double z)
+{
+       return (x*y - beta*z);
+}
